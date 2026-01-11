942ef9e8bdabf61e36646cd17407121d092dfa5d
pingpong59430-code <pingpong59430@gmail.com>
2026-01-11 12:52:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,30 @@
-// === SONS PS2 ===
+// === SONS ===
 const bootSound = new Audio("sounds/ps2.mp3");
 const clickSound = new Audio("sounds/click.mp3");
 
-// réglages
 bootSound.volume = 0.7;
 clickSound.volume = 0.7;
 
-// état
 let started = false;
 
-// écran de démarrage
 const startScreen = document.getElementById("startScreen");
 const menu = document.getElementById("menu");
 
-// clic pour démarrer la "console"
+// clic démarrage
 startScreen.addEventListener("click", () => {
   if (started) return;
   started = true;
 
-  // jouer le BOOT PS2
   bootSound.currentTime = 0;
   bootSound.play();
 
-  // cacher l'écran de démarrage après le son
   setTimeout(() => {
     startScreen.style.display = "none";
-    menu.style.display = "block";
-  }, 3000); // durée approx du son PS2
+    menu.style.display = "grid";
+  }, 3000);
 });
 
-// son de clic sur les jeux
+// clic sur les jeux
 document.addEventListener("click", (e) => {
   if (e.target.classList.contains("game-card")) {
     clickSound.currentTime = 0;
